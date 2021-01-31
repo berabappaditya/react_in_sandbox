80c1202d06976ca8b1eb817afb7dbf178da3c8a1
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2021-01-31 07:17:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="first">
+        <h2>My Top 3 favourite cartoon</h2>
+      </div>
+      <div id="second">
+          <div id="one">
+            <h4>Doraemon</h4>
+            <p>Doraemon is a fictional character in the Japanese manga and anime series of the same name created by Fujiko Fujio, the pen name of writing team Hiroshi Fujimoto and Motoo Abiko. He is a male robotic cat that travels back in time from the 22nd century to aid a preteen boy named Nobita.</p>
+          </div>
+     
+          <div id="two">
+          <h4>Ben 10</h4>
+            <p>Ben 10 is an American animated television series and media franchise created by Man of Action Studios and produced by Cartoon Network Studios. The series centers on a boy named Ben Tennyson who acquires the Omnitrix, an alien device resembling a wristwatch, which contains DNA of different alien species.</p>
+          </div>
+          
+          <div id="three">
+          <h4>Dragon Ball Z</h4>
+                <p>Thid is an American animated television series and media franchise created by Man of Action Studios and produced by Cartoon Network Studios. The series centers on a boy named Ben Tennyson who acquires the Omnitrix, an alien device resembling a wristwatch, which contains DNA of different alien species.</p>
+          </div>
+          </div>
+    </div>
+  );
+}
